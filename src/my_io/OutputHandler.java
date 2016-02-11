cf65d89cdaf3d8635afc88a7aa345ddcdfb2f440
nimroha <nimrod.morag@gmail.com>
2016-02-11 22:51:59 +0200

    hi

--- src/my_io/OutputHandler.java
+++ src/my_io/OutputHandler.java
@@ -1,5 +1,34 @@
 package my_io;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OutputHandler {
+	private List<Command> commands;
 	
+	public OutputHandler() {
+		commands = new ArrayList<Command>();
+	}
+	
+	public void toFile(String fileName){
+		try{
+			File file = new File(fileName+".txt");
+			FileWriter writer = new FileWriter(file);
+			writer.write(commands.size() + "\n");
+			for (Command c : commands){
+				writer.write(c.toString() + "\n");
+			}
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void addCom(Command c){
+		commands.add(c);
+	}
 }
